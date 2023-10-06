e33c49dfe2e76a4e311b2220399cc6d849582c00
Faran <faran-mehmood@hotmail.com>
2023-10-06 11:40:16 +0500

    Snack Store

---
+++ SnackShop/SnackShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackShopApp.swift
+//  SnackShop
+//
+//  Created by Maliks on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnackShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
